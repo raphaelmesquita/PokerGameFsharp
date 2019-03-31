e88dc05932bb913506e2a90cce2636231b3ef291
Raphael Lima <rmesquita@gmail.com>
2019-03-31 15:57:02 -0300

    refatoração do código

--- ConsoleApp1/Program.fs
+++ ConsoleApp1/Program.fs
@@ -8,9 +8,9 @@ type Rank = Two | Three | Four | Five | Six | Seven | Eight
 
 type Suit = Clubs | Diamonds | Spades | Hearts  
 
-type Card = Card of Rank * Suit 
+type Card = Rank * Suit 
 
-type Deck = Deck of Card list
+type Deck = Card list
 
 
 let createDeck () =
@@ -24,20 +24,19 @@ let createDeck () =
         for suit in suits do 
             cardList <-  cardList @ [ Card(rank, suit) ]
 
-    Deck(cardList)
+    cardList
 
 let sortDeck (deck: Deck) = 
-    let (Deck(cardList)) =  deck
-    Deck(List.sortBy (fun _ -> Guid.NewGuid().ToString()) cardList)
+    List.sortBy (fun _ -> Guid.NewGuid().ToString()) deck
 
 
 [<EntryPoint>]
 let main argv =
     let mutable count = 0 
-    let (Deck(cardList)) = createDeck() |> sortDeck
-    for card in cardList do 
+    let deck = createDeck() |> sortDeck
+    for card in deck do 
         count <- count + 1
-        let (Card(rank, suit)) = card
+        let rank, suit = card
         printfn "%d - %A %A " count rank suit
 
     Console.ReadLine()|> ignore
